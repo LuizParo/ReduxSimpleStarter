5dfb54fe32aba28079581ca52ffeb33d1cba91cd
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-01-13 20:31:17 -0200

    upgrading SearchBar component from function component to class-based component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,12 @@
 'use strict';
 
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => <input />;
+class SearchBar extends Component {
+    
+    render() {
+        return <input />;
+    }
+}
 
 export default SearchBar;
