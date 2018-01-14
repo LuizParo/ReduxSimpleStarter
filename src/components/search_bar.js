382b3d7a0c4237c4afc23ebc753c178cb54011b1
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-01-14 13:43:32 -0200

    adding 'state' property on search bar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,6 +3,11 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { term : '' };
+    }
     
     render() {
         return <input onChange={this.onInputChange} />;
