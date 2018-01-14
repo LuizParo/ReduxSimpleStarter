d748a9523a95ff8c99292bda572a71114decd406
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-01-14 15:39:55 -0200

    merge

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
@@ -8,18 +8,25 @@ import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBlrpOxVV2_zY6fJUZ4o6i9MknyExVoEUI';
 
-YTSearch({
-    key : API_KEY,
-    term : 'surfboards'
-}, console.log);
-
 // Create a new component. This component should produce some HTML.
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = { videos : [] };
+        
+        YTearch({ key : API_KEY, term : 'surfboards' }, videos => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 };
 
 // Take this component's generated HTML and put it on the page (in the DOM).
