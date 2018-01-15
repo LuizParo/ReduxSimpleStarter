7b2eeca05fbc690615f4eb0ef8a503e263d1ec58
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-01-14 23:09:20 -0200

    applying some css

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,7 +11,7 @@ class SearchBar extends Component {
     
     render() {
         return (
-            <div>
+            <div className="search-bar">
                 <input
                     onChange={this.onInputChange.bind(this)}
                     value={this.state.term} />
