e4852117654d2eeca69e1c0740423eb9a7115705
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-01-15 21:57:42 -0200

    creating ActiveBookReducer and binding it to the application state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@
 
 import { combineReducers } from 'redux';
 
+import ActiveBookReducer from './reducer_active_book';
 import BooksReducer from './reducer_books';
 
 const rootReducer = combineReducers({
-    books : BooksReducer
+    books : BooksReducer,
+    activeBook : ActiveBookReducer
 });
 
 export default rootReducer;
