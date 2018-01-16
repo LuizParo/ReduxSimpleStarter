9c63ddad8a0255c096ed3ec807d2ea8b02d291dc
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-01-16 20:31:36 -0200

    setting up the weather action generator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,18 @@
 'use strict';
 
+import axios from 'axios';
+
 const API_KEY = '9293c4b15bfae165847567d9f6a3c2ec';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}?q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type : FETCH_WEATHER,
+        payload : request
+    };
+}
