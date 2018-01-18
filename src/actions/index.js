9699d14e1ffd6b98391befc60901d9de43c682c4
Luiz Guilherme Paro <lparo@daitangroup.com>
2018-01-18 11:51:40 -0200

    creating action creator for posts

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import axios from 'axios';
+
+export const FETCH_POSTS = 'fetch_posts';
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=luizparo010791';
+
+export function fetchPosts() {
+    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+
+    return {
+        type : FETCH_POSTS,
+        payload : request
+    };
+}
