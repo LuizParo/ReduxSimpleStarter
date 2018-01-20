046834e10109cc6983d73ff7557f9b6f0920841e
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-01-20 21:38:39 -0200

    mapping a single post from the posts list in the PostsShow component

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,14 +1,28 @@
 'use strict';
 
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
 
+    componentDidMount() {
+        const { id } = this.props.match.params;
+        this.props.fetchPost(id);
+    }
+
     render() {
         return (
-            <div>Posts show!</div>
+            <div>
+                Posts show!
+            </div>
         );
     }
 }
 
-export default PostsShow;
+function mapStateToProps({ posts }, ownProps) {
+    return { post : posts[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostsShow);
