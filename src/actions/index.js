4a038dfb15f49decd4f98da6a44c6bd0e4ae0c4d
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-01-20 21:23:08 -0200

    creating an action creator to fetch a single post data

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POST = 'fetch_post';
 export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -13,6 +14,15 @@ export function fetchPosts() {
 
     return {
         type : FETCH_POSTS,
+        payload : request
+    };
+}
+
+export function fetchPost(id) {
+    const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+    return {
+        type : FETCH_POST,
         payload : request
     };
 }
