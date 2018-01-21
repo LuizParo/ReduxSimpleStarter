f489d1530c2521d71f01eb8bcb8aaedd8201908d
Luiz Guilherme Paro <lg_paro@hotmail.com>
2018-01-21 11:41:58 -0200

    caching the list of posts when PostsShow component is rendered and creating a Link on PostsIndex omponent to navigate to a specific post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { fetchPost } from '../actions';
@@ -8,8 +9,10 @@ import { fetchPost } from '../actions';
 class PostsShow extends Component {
 
     componentDidMount() {
-        const { id } = this.props.match.params;
-        this.props.fetchPost(id);
+        if (!this.props.post) {
+            const { id } = this.props.match.params;
+            this.props.fetchPost(id);
+        }
     }
 
     render() {
@@ -21,6 +24,7 @@ class PostsShow extends Component {
 
         return (
             <div>
+                <Link to="/">Back to Index</Link>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
